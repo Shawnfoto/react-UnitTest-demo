335b1e59a703e1b99af75ebd01db83a055917ba2
研發部 徐偉翔 <rpd11140@gmail.com>
2018-10-04 18:43:56 +0800

    reducer test code not yet

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { comment: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ comment: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.saveComment(this.state.comment);
+    this.setState({ comment: "" });
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a Comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <div>
           <button>Submit Comment</button>
         </div>
@@ -13,4 +32,7 @@ class CommentBox extends Component {
     );
   }
 }
-export default CommentBox;
+export default connect(
+  null,
+  actions
+)(CommentBox);
