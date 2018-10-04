79b6615f0a15e22a1e0b2b928546ccf37ea70262
Shawn <rpd11140@gmail.com>
2018-10-05 00:23:36 +0800

    action test done

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import CommentBox from "./CommentBox";
 import CommentList from "./CommentList";
+
 class App extends Component {
   render() {
     return (
