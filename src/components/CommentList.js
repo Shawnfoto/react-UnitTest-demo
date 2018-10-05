b03946b48483b95a3e2dfd09d8159bb7ecbf1ce2
Shawn <rpd11140@gmail.com>
2018-10-05 20:56:25 +0800

    unit test react+redux+call api done

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,4 +1,24 @@
-import React from "react";
-export default () => {
-  return <div>Comment List</div>;
-};
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+  render() {
+    return <ul>{this.renderComments()}</ul>;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    comments: state.comments
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(CommentList);
